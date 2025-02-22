49b40d0e5ae93c1e8db6a7de89009f7da5ee09c8
Anusree <anilkumaranusree113@gmail.com>
2025-02-22 22:56:46 +0530

    fix: ui animation home page - projects section

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import Image from "next/image";
 import styles from "@/styles/home.module.scss";
+import Link from "next/link";
 
 export default function Projects() {
+  const featuredProjects = [
+    {
+      name: "Ecommerce",
+      techstack: ["React", "NodeJS", "MongoDB"],
+      link: "https://ebazar-fullstack.vercel.app/home",
+      image: "/assets/images/projects/ecommerce-app.png",
+    },
+    {
+      name: "Music",
+      techstack: ["React", "NodeJS", "Material UI"],
+      link: "https://github.com/Anusree6154s/qtify-frontend",
+      image: "/assets/images/projects/music-app.png",
+    },
+    {
+      name: "Movies",
+      techstack: ["React", "NodeJS", "MongoDB", "Tailwind CSS"],
+      link: "https://github.com/Anusree6154s/xflix-frontend",
+      image: "/assets/images/projects/movies-app.png",
+    },
+    {
+      name: "Blog",
+      techstack: ["Jekyll", "SCSS", "Github Pages"],
+      link: "https://anusree6154s.github.io/documentation/",
+      image: "/assets/images/projects/blog-app.png",
+    },
+  ];
+
   return (
     <section className={styles.projects}>
       <div className={styles["projects-inner"]}>
@@ -24,103 +52,35 @@ export default function Projects() {
             </div>
           </div>
         </div>
-
-        <div className={styles["project-items"]}>
-          <div className={styles.heading}>
-            <div className={styles.title}>
-              <p className={styles["title-inner"]}>Ecommerce</p>
+        {featuredProjects.map((item, i) => (
+          <div key={i} className={styles["project-items"]}>
+            <div className={styles.heading}>
+              <div className={styles.title}>
+                <p className={styles["title-inner"]}>{item.name}</p>
+              </div>
+              <div className={styles.techstack}>
+                <p className={styles["techstack-inner"]}>
+                  {item.techstack.map((stack, i2) => (
+                    <span key={i2}>{stack}</span>
+                  ))}
+                </p>
+              </div>
             </div>
-            <div className={styles.techstack}>
-              <p className={styles["techstack-inner"]}>
-                <span>React</span>
-                <span>NodeJS</span>
-                <span>MongoDB</span>
-              </p>
-            </div>
-          </div>
-          <div className={styles.img}>
-            <div className={styles["img-inner"]}>
-              <Image
-                src="/assets/images/projects/ecommerce-app.png"
-                alt="Landscape picture"
-                width={500}
-                height={300}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles["project-items"]}>
-          <div className={styles.heading}>
-            <div className={styles.title}>
-              <p className={styles["title-inner"]}>Music</p>
-            </div>
-            <div className={styles.techstack}>
-              <p className={styles["techstack-inner"]}>
-                <span>React</span>
-                <span>NodeJS</span>
-              </p>
+            <div className={styles.img}>
+              <div className={styles["img-inner"]}>
+                <Link href={item.link}>
+                  <Image
+                    src={item.image}
+                    alt="Landscape picture"
+                    width={500}
+                    height={300}
+                  />
+                </Link>
+              </div>
             </div>
           </div>
-          <div className={styles.img}>
-            <div className={styles["img-inner"]}>
-              <Image
-                src="/assets/images/projects/music-app.png"
-                alt="Landscape picture"
-                width={500}
-                height={300}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles["project-items"]}>
-          <div className={styles.heading}>
-            <div className={styles.title}>
-              <p className={styles["title-inner"]}>Movies</p>
-            </div>
-            <div className={styles.techstack}>
-              <p className={styles["techstack-inner"]}>
-                <span>React</span>
-                <span>NodeJS</span>
-                <span>MongoDB</span>
-              </p>
-            </div>
-          </div>
-          <div className={styles.img}>
-            <div className={styles["img-inner"]}>
-              <Image
-                src="/assets/images/projects/movies-app.png"
-                alt="Landscape picture"
-                width={500}
-                height={300}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles["project-items"]}>
-          <div className={styles.heading}>
-            <div className={styles.title}>
-              <p className={styles["title-inner"]}>Blog</p>
-            </div>
-            <div className={styles.techstack}>
-              <p className={styles["techstack-inner"]}>
-                <span>Jekyll</span>
-                <span>SCSS</span>
-              </p>
-            </div>
-          </div>
-          <div className={styles.img}>
-            <div className={styles["img-inner"]}>
-              <Image
-                src="/assets/images/projects/blog-app.png"
-                alt="Landscape picture"
-                width={500}
-                height={300}
-              />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-      {/* </div> */}
     </section>
   );
 }
